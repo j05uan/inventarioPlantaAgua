93fca166571e94f1529b524d524b211f457d3098
j05uan <lopezamayajuanpablo@gmail.com>
2024-12-11 20:56:56 -0500

    Arreglos del Cors

--- plantaagua/src/main/java/plantaagua/plantaagua/controller/ContactController.java
+++ plantaagua/src/main/java/plantaagua/plantaagua/controller/ContactController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
 import plantaagua.plantaagua.entity.Contact;
 import plantaagua.plantaagua.repository.ContactRepository;
 import plantaagua.plantaagua.service.ContactService;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,14 +22,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
 
-
-
+@AllArgsConstructor
 @RequestMapping("/api/contacts")
 @RestController
+// @CrossOrigin(origins = "http://localhost:4200")
 public class ContactController {
 
-    @Autowired
-    private ContactService contactService;
+    
+    private final ContactService contactService;
+
+    
 
     @GetMapping    
     public Iterable<Contact> list(){
@@ -36,33 +40,33 @@ public class ContactController {
 
     @GetMapping("{id}")
     public Contact get(@PathVariable Integer id) {
-        return contactRepository.findById(id).orElse(null);
+        return contactService.findById(id);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Contact create(@RequestBody Contact contact) {
         //Para asignarle la fecha y hora actual al valor 
-        contact.setCreatedAt(LocalDateTime.now()); 
-        return contactRepository.save(contact);
+        // contact.setCreatedAt(LocalDateTime.now()) 
+        return contactService.create(contact);
     } 
     
     @PutMapping("{id}")
     public Contact update(@PathVariable Integer id, @RequestBody Contact form) {
         //Se traen los datos de la base de datos, para sobre escribir sobre los datos que se desean cambiar, y no actualizar los datos que no deben ser actualizados
-        Contact contactFromDb = contactRepository.findById(id).orElse(null);
-        //Se asignan los valores que se envian el usuario para actualzar
-        contactFromDb.setName(form.getName());
-        contactFromDb.setEmail(form.getEmail());
+        // Contact contactFromDb = contactRepository.findById(id).orElse(null);
+        // //Se asignan los valores que se envian el usuario para actualzar
+        // contactFromDb.setName(form.getName());
+        // contactFromDb.setEmail(form.getEmail());
 
-        return contactRepository.save(contactFromDb);
+        return contactService.update(id,form);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("{id}")
     public void delete(@PathVariable Integer id) {
-        Contact contactFromDb = contactRepository.findById(id).orElse(null);
-        contactRepository.delete(contactFromDb);
+        // Contact contactFromDb = contactRepository.findById(id).orElse(null);
+        contactService.delete(id);
     }
 
 }
--- plantaagua/src/main/java/plantaagua/plantaagua/service/ContactService.java
+++ plantaagua/src/main/java/plantaagua/plantaagua/service/ContactService.java
@@ -1,22 +1,49 @@
 package plantaagua.plantaagua.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import lombok.AllArgsConstructor;
 import plantaagua.plantaagua.entity.Contact;
 import plantaagua.plantaagua.repository.ContactRepository;
 
+
+@AllArgsConstructor
 @Service
 public class ContactService {
 
-    @Autowired
-    private ContactRepository contactRepository; 
+    private final ContactRepository contactRepository; 
 
     public Iterable<Contact> findAll(){
         return contactRepository.findAll();
     }
 
-    // public Contact create(Contact contact){
-        
-    // }
+    public Contact findById(Integer id) {
+        return contactRepository.findById(id).orElse(null);
+    }
+
+
+    public Contact create(Contact contact){
+        contact.setCreatedAt(LocalDateTime.now()); 
+        return contactRepository.save(contact);
+    }
+
+    public Contact update(Integer id, Contact form) {
+        //Se traen los datos de la base de datos, para sobre escribir sobre los datos que se desean cambiar, y no actualizar los datos que no deben ser actualizados
+        Contact contactFromDb = findById(id);
+        //Se asignan los valores que se envian el usuario para actualzar
+        contactFromDb.setName(form.getName());
+        contactFromDb.setEmail(form.getEmail());
+
+        return contactRepository.save(contactFromDb);
+    }
+
+    public void delete(Integer id) {
+        Contact contactFromDb = findById(id);
+        contactRepository.delete(contactFromDb);
+    }
+
 }
